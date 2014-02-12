b7d9686d525890bacf0452d3d678d699f187b3d4
John Otander <johnotander@gmail.com>
2014-02-12 12:13:24 -0700

    Fix form feedback.

--- js/emailer.js
+++ js/emailer.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   Parse.initialize("03aUjJu9Fz7GmmE4QZDNnAwmUZA8VxeZe3Jf1J7Z", "Pl2oGJ3BjWx3J0fcwsH6ztSLqY4mxtb9tF04c6Dv");
 
   // Setup the form to watch for the submit event
-  $('#contact-submit').submit(function(e){
+  $('form.contact').submit(function(e){
     e.preventDefault();
 
     // Grab the elements from the form to make up
@@ -15,18 +15,25 @@ $(document).ready(function(){
       subject: document.getElementById('subject').value,
       message: document.getElementById('message').value
     }
+
+    happy_p = '<p id="result-msg">Thanks for getting in touch. <br> We\'ll get back to you as soon as possible.</p><br><br>';
+    sad_p = '<p id="result-msg">Sorry about that, something went wrong.</p><br><br>';
+
     // Run our Parse Cloud Code and
     // pass our 'data' object to it
     Parse.Cloud.run("sendEmail", data, {
       success: function(object) {
-        $('#response').html('Email sent!').addClass('success').fadeIn('fast');
+        $('form.contact').prepend(happy_p);
       },
 
       error: function(object, error) {
         console.log(error);
-        $('#response').html('Error! Email not sent!').addClass('error').fadeIn('fast');
+        $('form.contact').prepend(sad_p);
+        $('form.contact').attr('disabled', false).removeClass('disabled');
       }
     });
+
+    return false;
   });
 
 });
