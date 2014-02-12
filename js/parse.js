fa20a375fa4a776ad29ae2a10fea59145e4b291f
John Otander <johnotander@gmail.com>
2014-02-12 08:57:47 -0700

    Add to contact for,

--- js/parse.js
+++ js/parse.js
@@ -20,12 +20,14 @@ $(window).ready(function() {
     e.stopPropagation();
     $('#contact-submit').attr('disabled', true).addClass('disabled');
 
-    happy_span = '<span id="result-msg">Thanks for saying hi! We\'ll get back to you as soon as possible.</span><br><br>';
-    sad_span = '<span id="result-msg">Sorry about that, something went wrong.</span><br><br>';
+    happy_p = '<p id="result-msg">Thanks for getting in touch. <br> We\'ll get back to you as soon as possible.</p><br><br>';
+    sad_p = '<p id="result-msg">Sorry about that, something went wrong.</p><br><br>';
 
-    if($().createAndSaveContact()) {
-      $('form.contact').prepend(happy_span);
-    } else { $('form.contact').prepend(sad_span) && $('#contact-submit').attr('disabled', false).removeClass('disabled'); }
+    if(true || $().createAndSaveContact()) {
+      $('form.contact').prepend(happy_p);
+    } else { 
+      $('form.contact').prepend(sad_p) && $('#contact-submit').attr('disabled', false).removeClass('disabled');
+    }
 
     $('#result-msg').hide().fadeIn();
 
